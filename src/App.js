db2b2c5281f005e5839a6cd4ff7bb6fd2a623b05
Adam Schachne <adamschachne@live.com>
2018-08-04 16:40:10 -0700

    cat and mouse render props with HOC

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import CatWithMouse from './Cat';
 
-class App extends Component {
+const Cat = ({ mouse, name }) => (
+  <div style={{ position: 'absolute', left: mouse.x - 130, top: mouse.y - 90 }}>
+    <img src="/cat.jpg" alt="cat" />
+    <h4 style={{ textAlign: 'center' }}>
+      {name}
+    </h4>
+  </div>
+);
+
+function withMouse(Component, name) {
+  return {
+    [name]: class extends React.Component {
+      render() {
+        return (
+          <Mouse render={mouse => (
+            <Component mouse={mouse} {...this.props} />
+          )} />
+        );
+      }
+    }
+  }[name];
+}
+
+const CatWithMouse = withMouse(Cat, "CatWrapper");
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
+        {this.props.render(this.state)}
       </div>
     );
   }
 }
 
-export default App;
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <div style={{ height: '100%' }}>
+        <h1>Move the mouse around!</h1>
+        {/* <Mouse render={mouse => (
+          <Cat mouse={mouse} />
+        )} /> */}
+        <CatWithMouse name="Anthony" />
+      </div>
+    );
+  }
+}
+
+export default MouseTracker;
---
+++ src/Cat.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+// If you really want a HOC for some reason, you can easily
+// create one using a regular component with a render prop!
+function withMouse(Component) {
+  return class extends React.Component {
+    render() {
+      return (
+        <Mouse render={mouse => (
+          <Component {...this.props} mouse={mouse} />
+        )}/>
+      );
+    }
+  }
+}
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
+        {this.props.render(this.state)}
+      </div>
+    );
+  }
+}
+
+class Cat extends React.Component {
+  render() {
+    const mouse = this.props.mouse;
+    return (
+      <img src="/cat.jpg" style={{ position: 'absolute', left: mouse.x - 130, top: mouse.y - 90 }} />
+    );
+  }
+}
+
+export default withMouse(Cat);
